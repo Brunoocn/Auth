869db2a554c37fd8f99f531aace9385a1ca1177c
Bruno Caneo <brunocaneo3@gmail.com>
2022-03-14 23:19:13 -0300

    Adding styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../contexts/AuthContext";
 
+import '../styles/globals.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { parseCookies } from 'nookies'
-import styles from "../styles/Home.module.css";
 import { withSSRGuest } from "../utils/withSSRGuest";
+import styles from "./Home.module.scss";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -23,24 +21,33 @@ export default function Home() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.container}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Entrar</button>
-    </form>
+    <>
+    <img src="/vector-l.svg" alt="bubble" className={styles.bubble_left} />
+    <img src="/vector-r.svg" alt="bubble" className={styles.bubble_right} />
+      <h1 className={styles.title}>Sign In</h1>
+        <form onSubmit={handleSubmit} className={styles.container}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+
+          <button type="submit">Entrar</button>
+        </form>
+    </>
   );
 }
 
 export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
-    props: {}
-  }
+    props: {},
+  };
 });
